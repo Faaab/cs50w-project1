f3b55ce11c6c7328a2077db2f3caf07570b46125
Faaab <35370400+Faaab@users.noreply.github.com>
2018-09-04 19:14:28 +0200

    Implemented check_password

--- helpers.py
+++ helpers.py
@@ -3,6 +3,13 @@
 import uuid
 import hashlib
 
+#Takes string, hashes it with SHA256 and random salt, returns a hashed string ending with the salt
 def hash_password(password):
     salt = uuid.uuid4().hex
-    return hashlib.sha256(salt.encode() + password.encode()) + ':' + salt
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+#Takes a hashed password (from password DB) and a password provided by the user
+#Hashes second password with same salt, and returns true if hashes match; false otherwise
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
