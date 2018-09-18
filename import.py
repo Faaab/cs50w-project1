1bc2cf6a0157c7a65d49dd9db171e2e67745f93d
Faaab <35370400+Faaab@users.noreply.github.com>
2018-09-18 07:30:42 +0200

    added crucial import

--- import.py
+++ import.py
@@ -1,3 +1,5 @@
+import csv
+
 """In this file, you will find a function that imports all data from a .csv-file to the PostgreSQL
 database that is linked to application.py. It is not part of the webapp contained in application.py,
 and must be run from the CLI with 'python3 import'."""
