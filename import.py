5f92422b1bf9b579e74a67a263e0e25890277d64
Faaab <35370400+Faaab@users.noreply.github.com>
2018-09-18 07:03:51 +0200

    Created file for importing data from .csv; added description

---
+++ import.py
@@ -0,0 +1,5 @@
+"""In this file, you will find a function that imports all data from a .csv-file to the PostgreSQL
+database that is linked to application.py. It is not part of the webapp contained in application.py,
+and must be run from the CLI with 'python3 import'."""
+
+# TODO TODO TODO TODO TODO TODO
