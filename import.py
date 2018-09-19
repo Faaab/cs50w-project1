7dfb95f94493575a9206f3f8de830fc735223816
Faaab <35370400+Faaab@users.noreply.github.com>
2018-09-19 19:34:27 +0200

    Added relevant SQLAlchemy code for database-connection.

--- import.py
+++ import.py
@@ -4,12 +4,15 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-"""In this file, you will find a function that imports all data from a .csv-file to the PostgreSQL
-database that is linked to application.py. It is not part of the webapp contained in application.py,
+"""In 'main', you will find a function that imports all data from a .csv-file to the PostgreSQL
+database that is linked to with the environment variable "DATABASE_URL".
+It is specifically made to import relevant data for the 'books' app (CS50W Project 1),
 and must be run from the CLI with 'python3 import'."""
 
-# TODO TODO TODO TODO TODO TODO
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
 
+# TODO TODO TODO TODO TODO TODO
 # db.execute("CREATE TABLE books (
 #     isbn INTEGER PRIMARY KEY,
 #     title VARCHAR NOT NULL,
@@ -17,15 +20,12 @@ and must be run from the CLI with 'python3 import'."""
 #     year INTEGER NOT NULL
 # )")
 
-def main:
+def main():
     with open('books.csv', 'r') as books_csv:
         csv_reader = csv.reader(books_csv)
 
         for isbn, title, author, year in csv_reader:
-            print(isbn)
-            print(title)
-            print(author)
-            print(year)
+            print(f"Added row to table: isbn = {isbn}, title = {title}, author = {author}, year = {year}. JK.")
 
 if __name__ == "__main__":
     main()
