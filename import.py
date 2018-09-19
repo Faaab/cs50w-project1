a282e1112f90e5fa61fa8d9c09cb7b9af77e50cc
Faaab <35370400+Faaab@users.noreply.github.com>
2018-09-19 19:10:20 +0200

    Added test code to read all lines from csv file

--- import.py
+++ import.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 """In this file, you will find a function that imports all data from a .csv-file to the PostgreSQL
 database that is linked to application.py. It is not part of the webapp contained in application.py,
@@ -6,9 +7,15 @@ and must be run from the CLI with 'python3 import'."""
 
 # TODO TODO TODO TODO TODO TODO
 
-db.execute("CREATE TABLE books (
-    isbn INTEGER PRIMARY KEY,
-    title VARCHAR NOT NULL,
-    author VARCHAR NOT NULL,
-    year INTEGER NOT NULL
-)")
+# db.execute("CREATE TABLE books (
+#     isbn INTEGER PRIMARY KEY,
+#     title VARCHAR NOT NULL,
+#     author VARCHAR NOT NULL,
+#     year INTEGER NOT NULL
+# )")
+
+with open('books.csv', 'r') as books_csv:
+    csv_reader = csv.reader(books_csv)
+
+    for line in csv_reader:
+        print(line, sys.stderr)
